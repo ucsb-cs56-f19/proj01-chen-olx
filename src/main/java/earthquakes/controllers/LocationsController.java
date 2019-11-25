bbb576e7a71b847868db4f8d137b942143fd97cb
Chen Xu <chen_xu@cs.ucsb.edu>
2019-11-24 17:14:56 -0800

    set fake location query service

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
 import java.util.HashMap;
 
-import earthquakes.services.EarthquakeQueryService;
+import earthquakes.services.LocationQueryService;
 import earthquakes.searches.LocSearch;
 import earthquakes.geojson.FeatureCollection;
 
@@ -32,8 +32,11 @@ public class LocationsController {
     @GetMapping("/locations/results")
     public String getLocationsResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
             LocSearch locSearch) {
+        LocationQueryService e =
+                new LocationQueryService();
         model.addAttribute("locSearch", locSearch);
-        // TODO: Actually do the search here and add results to the model
+        String json = e.getJSON(locSearch.getLocation());
+        model.addAttribute("json", json);
         return "locations/results";
     }
 }
